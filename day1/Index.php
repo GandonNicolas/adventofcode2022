558d1833e8b02822bc9f251ecbf75f128a7c9775
Nicolas Gandon <ngandon@umanit.fr>
2022-12-03 11:29:07 +0100

    Reponse day 2

--- day1/Index.php
+++ day1/Index.php
@@ -6,7 +6,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 class Index
 {
 
-    public function transformInput(): int
+    public function reponse1(): int
     {
         $input = file_get_contents('input.txt');
 
@@ -18,12 +18,34 @@ class Index
             if ($valeurTempo > $resultat) {
                 $resultat = $valeurTempo;
             }
-
         }
+
+        return $resultat;
+    }
+
+    public function reponse2(): int
+    {
+        $input = file_get_contents('input.txt');
+
+        $inputClean = explode("\n\n", $input);
+
+        $newArray = [];
+        foreach ($inputClean as $a) {
+            $newArray[] = array_sum(explode("\n", $a));
+        }
+        $resultat = 0;
+        $i = 0;
+        do {
+            $resultat += max($newArray);
+            unset($newArray[array_search(max($newArray), $newArray, true)]);
+            $i++;
+        } while ($i < 3);
 
         return $resultat;
     }
 }
 
 $class = new Index();
-echo $class->transformInput();
+echo $class->reponse1();
+echo "\n";
+echo $class->reponse2();
